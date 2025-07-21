6059588e64dc42b6d1b07bf3c35fffb6faaadee1
unknown <rafael.eng.mecanic@gmail.com>
2025-07-21 11:15:33 -0300

    finalizando side e top bar

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -3,16 +3,24 @@ import { RouterOutlet } from '@angular/router';
 import { TopBarComponent } from '../components/top-bar/top-bar.component';
 import { SidebarComponent } from '../components/sidebar/sidebar.component';
 import { CommonModule } from '@angular/common';
+import { BottomNavComponent } from '../components/bottom-nav/bottom-nav.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TopBarComponent, SidebarComponent],
+  imports: [CommonModule, RouterOutlet, TopBarComponent, SidebarComponent, BottomNavComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
 export class LayoutComponent {
   isSidebarCollapsed = false;
+
+  constructor() {
+    // Em telas pequenas, a sidebar começa sempre recolhida.
+    if (window.innerWidth < 768) {
+      this.isSidebarCollapsed = true;
+    }
+  }
 
   toggleSidebar() {
     this.isSidebarCollapsed = !this.isSidebarCollapsed;
