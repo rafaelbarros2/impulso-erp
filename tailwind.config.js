58d39d30f6ba50940cad243f8cbba712655c5627
unknown <rafael.eng.mecanic@gmail.com>
2025-08-14 12:43:34 -0300

    correção tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-import PrimeUI from 'tailwindcss-primeui';
+const PrimeUI = require('tailwindcss-primeui');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -16,11 +16,8 @@ module.exports = {
      preflight: false, // Desabilita o reset CSS do Tailwind para não conflitar com PrimeNG
    },
    safelist: [
-    { pattern: /^(w|h|min-h|max-w|max-h)-\[.*\]$/ },   // ex: h-[50vh]
     { pattern: /^(grid|flex|items-|justify-|gap-).+/ },
     { pattern: /^(bg|text|border)-(primary|red|gray)-\d+$/ },
-    { pattern: /^md:.+$/ }, { pattern: /^lg:.+$/ }, { pattern: /^xl:.+$/ },
-    { pattern: /^(rounded|shadow|transform|rotate-).+/ },
-    { pattern: /^(group|group-hover):.+$/ }
+    { pattern: /^(rounded|shadow|transform|rotate-).+/ }
   ]
 };
